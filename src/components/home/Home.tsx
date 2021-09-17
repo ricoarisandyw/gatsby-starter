733efbcfbf552a57ce27a877b47bf1ad252573a2
Rico Arisandy <arisandyrico@gmail.com>
2021-09-17 07:54:51 +0700

    Fix typo content

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -22,7 +22,7 @@ export default function Home() {
                     <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
                         <main className="mt-24 mx-auto max-w-7xl px-4 sm:mt-16 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                             <div className="mb-3">
-                                <span className="text-lg">remove distance better</span><br/>
+                                <span className="text-lg">remove distance between</span><br/>
                                 <h1 className="f-future header-label" data-aos="fade-right">Virtual World and Reality</h1>
                                 <span className="text-lg">Digital world is safe, easy to modify, and must help us learn real world.</span><br/>
                                 <span className="text-lg">You don't need to purchase tools again, we can download it.</span><br/>
--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -13,26 +13,26 @@ export default function Team(){
             <div className="p-6 bg-team rounded-xl text-center">
                 <div>
                     <span className="text-3xl f-future text-white">OUR TEAMS</span><br/>
-                    <span className="text-white">Our team have real profesionnal experencie</span>
+                    <span className="text-white">Our team have real profesionnal experience</span>
                 </div>
                 <div className="grid grid-col-1 lg:grid-cols-3">
                     <div className="text-center text-white p-6">
                         <div data-aos="flip-left" className="rounded-full h-36 w-36 mx-auto" style={{background:`url("${ilham}")`, backgroundSize:"cover"}} />
                         <span className="text-3xl f-future">CTO</span><br/>
                         <span className="">Ilham Achmad Al Hafidz</span><br/>
-                        <span>Master Student at PENS, XR Developer at Universitas Nahdlatul Ulama Surabaya</span><br/>
+                        <span>Master Student at PENS<br/>XR Developer at Universitas Nahdlatul Ulama Surabaya</span><br/>
                     </div>
                     <div className="text-center text-white p-6">
                         <div data-aos="flip-left" className="rounded-full h-36 w-36 mx-auto" style={{background:`url("${aziz}")`, backgroundSize:"cover"}} />
                         <span className="text-3xl f-future">CEO</span><br/>
-                        <span className="">Abdul Aziz</span><br/>
-                        <span>MBA Candidate at Nexford University, US. Cloud Consultant at Cloud Comrade Pte. Ltd</span><br/>
+                        <span className="">Abdul Azis</span><br/>
+                        <span>MBA Candidate at Nexford University<br/>US. Cloud Consultant at Cloud Comrade Pte. Ltd</span><br/>
                     </div>
                     <div className="text-center text-white p-6">
                         <div data-aos="flip-left" className="rounded-full h-36 w-36 mx-auto" style={{background:`url("${tafaquh}")`, backgroundSize:"cover"}} />
                         <span className="text-3xl f-future">CPMO</span><br/>
                         <span className="">M Tafaquh Fiddin Al Islami</span><br/>
-                        <span>Master Student at PENS,Software Engineer at Ruangguru</span><br/>
+                        <span>Master Student at PENS<br/>Software Engineer at Ruangguru</span><br/>
                     </div>
                 </div>
                 <div className="grid grid-col-1 lg:grid-cols-6">
